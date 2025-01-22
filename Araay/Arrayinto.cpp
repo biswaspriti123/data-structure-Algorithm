4478655efb3c663deec116df13243c6e097599f4
biswaspriti123 <biswaspriti913@gmail.com>
2025-01-22 06:09:43 -0800

    Array declare

---
+++ Araay/Arrayinto.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void printArray()
+{
+
+}
+int main()
+{
+    //declare
+    int number[15];
+    
+    cout<< "Everything is fine" <<endl;
+    return 0;
+}
